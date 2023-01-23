f2eb91385edae5d8c0a6ea703f0b892216df516c
Pedro Laraburu <pedrolaraburu@gmail.com>
2023-01-22 21:11:38 -0300

    feat: add login verification

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,46 +1,80 @@
-import { Component, OnInit } from '@angular/core';
-import { UserService } from 'src/app/services/user.service';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { UserService } from "src/app/services/user.service";
+import { Router } from "@angular/router";
+import {
+    FormControl,
+    FormBuilder,
+    FormGroup,
+    Validators,
+} from "@angular/forms";
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: "app-login",
+    templateUrl: "./login.component.html",
+    styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit {
-  showLoginAlert: boolean = false;
-  showModal: boolean = false;
-  email: string = '';
-  password: string = '';
-  constructor(private usersService: UserService, private router: Router){}
+    showLoginAlert: boolean = false;
+    showModal: boolean = false;
+    logins: any = {};
+    loginForm: any;
+    userSubmitted: boolean = false;
+    constructor(
+        private _usersService: UserService,
+        private router: Router,
+        private fb: FormBuilder
+    ) {}
 
-  ngOnInit(): void {
-    console.log('On init.');
-  }
+    ngOnInit(): void {
+        this.createLoginForm();
+    }
 
+    loginUser() {
+        return (this.logins = {
+            email: this.email.value,
+            password: this.password.value,
+        });
+    }
 
-  captura() {
-    console.log(this.email, this.password);
-  }
+    createLoginForm() {
+        this.loginForm = this.fb.group({
+            email: new FormControl('', [
+                Validators.required,
+                Validators.email,
+            ]),
+            password: new FormControl('', [
+                Validators.required,
+                Validators.pattern(/^(\s+\S+\s*)*(?!\s).*$/),
+            ]),
+        });
+    }
 
+    mostra() {
+        alert('Função em construção!');
+    }
 
-  mostra() {
-    this.showModal = true;
-  }
+    onSubmitLogin(): void {
+        console.log(this.loginForm.value);
+        this.userSubmitted = true;
+        // this.usersService.getUsers().subscribe(res => {
+        //   const user = res.find((e: any)=> {
+        //     return e.email === this.email && e.password === this.password
+        //   });
+        //   if (user){
+        //     this.usersService.emitChange(true);
+        //     this.router.navigate(['/dashboard']);
+        //     this.showLoginAlert = false;
+        //   } else {
+        //     this.showLoginAlert = true;
+        //   }
+        // })
+    }
 
-  login(): void{
-    // this.usersService.getUsers().subscribe(res => {
-    //   const user = res.find((e: any)=> {
-    //     return e.email === this.email && e.password === this.password
-    //   });
-    //   if (user){
-    //     this.usersService.emitChange(true);
-    //     this.router.navigate(['/dashboard']);
-    //     this.showLoginAlert = false;
-    //   } else {
-    //     this.showLoginAlert = true;
-    //   }
-    // })
+    // Getters
+    get email() {
+        return this.loginForm.get("email").value as FormControl;
+    }
 
-  }
-
+    get password() {
+        return this.loginForm.get("password").value as FormControl;
+    }
 }
